a1f4657af453f0e0e8dfe3c8803814c6b545c45f
TheWeepingSage <kanabarmillen@gmail.com>
2019-06-01 01:45:52 +0530

    Revised main function for actuator model testing, and the data

---
+++ plot_simplified_state.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+array_tc = np.loadtxt("simplified_actuator_tc_data.csv", delimiter=",")
+array_minstep = np.loadtxt("simplified_actuator_minstep_data.csv", delimiter=",")
+
+plt.plot(array_tc[:, 0], array_tc[:, 3], label="tc step w_1")
+plt.plot(array_minstep[:, 0], array_minstep[:, 3], label="min step w_1")
+plt.legend()
+plt.show()
---
+++ simplified_actuator_minstep_test.py
@@ -0,0 +1,55 @@
+import numpy as np
+from constants_1U import PWM_FREQUENCY, CONTROL_STEP, v_A_Torquer, No_Turns
+import analytical_act_current as aac
+import min_step_sampling as min_step
+import time as timer
+
+start = timer.time()
+
+v_duty_cycle = np.array([3, 4, 5])*1e-3
+timeArr = min_step.getTime(v_duty_cycle, 15)
+num_instants_per_cycle = len(timeArr)
+num_cycles = int(CONTROL_STEP * PWM_FREQUENCY)
+
+time = np.zeros(1)
+for i in range(0, 2 * num_cycles):
+    time = np.concatenate((time, timeArr + i / PWM_FREQUENCY))
+
+w_array = np.zeros((len(time), 4))
+w_array[:, 0] = time
+I0 = np.zeros(3)
+time_period = 1/PWM_FREQUENCY
+
+def w_dot_BI(current, b):
+    v_mu = No_Turns*np.multiply(v_A_Torquer, current)
+    v_w_dot = np.cross(v_mu, b)
+    return v_w_dot
+
+
+def getMag_b(time):
+    v_mag_b = np.array([1, 2, 3])*1e-3*np.sin(time)
+    return v_mag_b
+
+
+for j in range(0, 2):
+    for k in range(j * int(num_cycles), (j + 1) * int(num_cycles)):
+        edgeCurrentArray = aac.getEdgeCurrent(v_duty_cycle, I0)
+        currentArray = np.zeros((3, 3))
+        print("cycle ", k + 1, "control step", j + 1)
+        for i in range(k * num_instants_per_cycle, (k + 1) * num_instants_per_cycle):
+            intTimeArr = np.linspace(time[i] % time_period, time[i+1] % time_period, 3, endpoint=True)
+            currentArray = aac.getCurrentList(v_duty_cycle, intTimeArr, 3, edgeCurrentArray)
+            h = time[i+1] - time[i]
+            w_bIb = w_array[i, 1:4]
+            # RK-4 routine
+            k1 = w_dot_BI(currentArray[0], getMag_b(intTimeArr[0])) * h
+            k2 = w_dot_BI(currentArray[1], getMag_b(intTimeArr[1])) * h
+            k3 = w_dot_BI(currentArray[1], getMag_b(intTimeArr[1])) * h
+            k4 = w_dot_BI(currentArray[2], getMag_b(intTimeArr[2])) * h
+            w_array[i+1, 1:4] = w_bIb + (k1 + 2*k2 + 2*k3 + k4)/6
+        I0 = edgeCurrentArray[len(edgeCurrentArray) - 1]
+
+
+np.savetxt("simplified_actuator_minstep_data.csv", w_array[:, :], delimiter=",")
+end = timer.time()
+print(end-start)
