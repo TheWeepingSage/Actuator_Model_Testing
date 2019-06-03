78df3b2f1f7892cdefc320ce554f6d44da0f547e
TheWeepingSage <kanabarmillen@gmail.com>
2019-06-03 19:46:04 +0530

    True data(with error)

--- simplified_actuator_true_data.py
+++ simplified_actuator_true_data.py
@@ -8,10 +8,12 @@ time_period = 1/PWM_FREQUENCY
 edgeCurrentArray_1 = aac.getEdgeCurrent(v_duty_cycle, np.zeros(3))
 edgeCurrentArray_2 = aac.getEdgeCurrent(v_duty_cycle, edgeCurrentArray_1[len(edgeCurrentArray_1)-1, :])
 w_array = np.zeros((len(edgeCurrentArray_1), 3))
-for i in range(0, int(len(edgeCurrentArray_1)/2)):
-    w_array[i + 1] = w_array[i] + (PWM_AMPLITUDE * time_period * v_A_Torquer / RESISTANCE) * (v_duty_cycle[1] - v_duty_cycle[2]) + (edgeCurrentArray_1[i * 2, 1] - edgeCurrentArray_1[i * 2, 2]) * v_A_Torquer * INDUCTANCE / RESISTANCE * (1 - np.exp(-RESISTANCE *time_period / INDUCTANCE))
-    print(w_array[i+1])
-for i in range(0, int(len(edgeCurrentArray_2)/2)):
-    w_array[i + 1] = w_array[i] + (PWM_AMPLITUDE * time_period * v_A_Torquer / RESISTANCE) * (v_duty_cycle[1] - v_duty_cycle[2]) + (edgeCurrentArray_1[i * 2, 1] - edgeCurrentArray_1[i * 2, 2]) * v_A_Torquer * INDUCTANCE / RESISTANCE * (1 - np.exp(-RESISTANCE *time_period / INDUCTANCE))
-    print(w_array[i+1])
-print(w_array[len(w_array)-1, :])
+for i in range(0, 2000):
+    w_array[i + 1] = w_array[i] + PWM_AMPLITUDE/RESISTANCE*time_period*(v_duty_cycle[1]-v_duty_cycle[2])
+    w_array[i + 1] = w_array[i + 1] - PWM_AMPLITUDE/RESISTANCE*INDUCTANCE/RESISTANCE*(np.exp(-time_period*(1 - v_duty_cycle[1])*RESISTANCE/INDUCTANCE)-np.exp(-time_period*(1-v_duty_cycle[2])*RESISTANCE/INDUCTANCE))
+    w_array[i + 1] = w_array[i + 1] + INDUCTANCE/RESISTANCE * (edgeCurrentArray_1[2*i, 1]-edgeCurrentArray_1[2*i, 2])*(1 - np.exp(-RESISTANCE*time_period/INDUCTANCE))
+for i in range(2000, 4000):
+    w_array[i + 1] = w_array[i] + PWM_AMPLITUDE/RESISTANCE*time_period*(v_duty_cycle[1]-v_duty_cycle[2])
+    w_array[i + 1] = w_array[i + 1] - PWM_AMPLITUDE/RESISTANCE*INDUCTANCE/RESISTANCE*(np.exp(-time_period*(1 - v_duty_cycle[1])*RESISTANCE/INDUCTANCE)-np.exp(-time_period*(1-v_duty_cycle[2])*RESISTANCE/INDUCTANCE))
+    w_array[i + 1] = w_array[i + 1] + INDUCTANCE/RESISTANCE * (edgeCurrentArray_2[2*i, 1]-edgeCurrentArray_1[2*i, 2])*(1 - np.exp(-RESISTANCE*time_period/INDUCTANCE))
+
