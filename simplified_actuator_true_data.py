3de9b75797bce289a94b7b7604b7fe47b3bdde4c
TheWeepingSage <kanabarmillen@gmail.com>
2019-06-04 12:58:19 +0530

    True data(with error)

--- simplified_actuator_true_data.py
+++ simplified_actuator_true_data.py
@@ -11,9 +11,12 @@ w_array = np.zeros((len(edgeCurrentArray_1), 3))
 for i in range(0, 2000):
     w_array[i + 1] = w_array[i] + PWM_AMPLITUDE/RESISTANCE*time_period*(v_duty_cycle[1]-v_duty_cycle[2])
     w_array[i + 1] = w_array[i + 1] - PWM_AMPLITUDE/RESISTANCE*INDUCTANCE/RESISTANCE*(np.exp(-time_period*(1 - v_duty_cycle[1])*RESISTANCE/INDUCTANCE)-np.exp(-time_period*(1-v_duty_cycle[2])*RESISTANCE/INDUCTANCE))
-    w_array[i + 1] = w_array[i + 1] + INDUCTANCE/RESISTANCE * (edgeCurrentArray_1[2*i, 1]-edgeCurrentArray_1[2*i, 2])*(1 - np.exp(-RESISTANCE*time_period/INDUCTANCE))
+    w_array[i + 1] = w_array[i + 1] + INDUCTANCE/RESISTANCE * (edgeCurrentArray_1[2*(i%2000), 1]-edgeCurrentArray_1[2*(i%2000), 2])*(1 - np.exp(-RESISTANCE*time_period/INDUCTANCE))
+    print(w_array[i+1]*1e-3)
 for i in range(2000, 4000):
     w_array[i + 1] = w_array[i] + PWM_AMPLITUDE/RESISTANCE*time_period*(v_duty_cycle[1]-v_duty_cycle[2])
     w_array[i + 1] = w_array[i + 1] - PWM_AMPLITUDE/RESISTANCE*INDUCTANCE/RESISTANCE*(np.exp(-time_period*(1 - v_duty_cycle[1])*RESISTANCE/INDUCTANCE)-np.exp(-time_period*(1-v_duty_cycle[2])*RESISTANCE/INDUCTANCE))
-    w_array[i + 1] = w_array[i + 1] + INDUCTANCE/RESISTANCE * (edgeCurrentArray_2[2*i, 1]-edgeCurrentArray_1[2*i, 2])*(1 - np.exp(-RESISTANCE*time_period/INDUCTANCE))
-
+    w_array[i + 1] = w_array[i + 1] + INDUCTANCE/RESISTANCE * (edgeCurrentArray_2[2*(i%2000), 1]-edgeCurrentArray_1[2*(i%2000), 2])*(1 - np.exp(-RESISTANCE*time_period/INDUCTANCE))
+    print(w_array[i+1]*1e-3)
+w_array = w_array * v_A_Torquer[0]
+print(w_array[len(w_array)-1])
