1d28cf18b60c3e89717f053b5123dc74cbcf2121
TheWeepingSage <kanabarmillen@gmail.com>
2019-06-04 19:55:16 +0530

    True angular velocity data

--- simplified_actuator_true_data.py
+++ simplified_actuator_true_data.py
@@ -7,7 +7,7 @@ duty = np.array([3, 4, 5])*1e-3
 t_p = 1/PWM_FREQUENCY
 edgeCurrent_1 = aac.getEdgeCurrent(duty, np.zeros(3))
 edgeCurrent_2 = aac.getEdgeCurrent(duty, edgeCurrent_1[len(edgeCurrent_1)-1, :])
-w_array = np.zeros((len(edgeCurrent_1), 3))
+w_array = np.zeros((len(edgeCurrent_1), 1))
 for i in range(0, 2000):
     w_array[i + 1] = w_array[i] + V_max/R*t_p*(3*duty[1]-2*duty[2])
     w_array[i + 1] = w_array[i + 1] - V_max/R*L/R*(3*np.exp(-t_p*(1 - duty[1])*R/L)-2*np.exp(-t_p*(1-duty[2])*R/L))
