9a548aabf56c51333238a1d052074ef03bcd81e3
TheWeepingSage <kanabarmillen@gmail.com>
2019-06-08 11:29:15 +0530

    Long simulations for testing actuator model - sampling method 1&2

--- simplified_actuator_minstep_long.py
+++ simplified_actuator_minstep_long.py
@@ -1,7 +1,7 @@
 import numpy as np
 from constants_1U import PWM_FREQUENCY, CONTROL_STEP, v_A_Torquer, No_Turns
 import analytical_act_current as aac
-import tc_step_sampling as tc
+import min_step_sampling as minstep
 import time as timer
 
 start = timer.time()
@@ -27,7 +27,7 @@ num_cycles_per_step = int(CONTROL_STEP/time_period)
 
 for i in range(0, num_steps):
     duty = np.array([i+1, (-1**i)*(i+2), i+3])*1e-3
-    timeArr = tc.getTimeArr(duty)
+    timeArr = minstep.getTime(duty, 15)
     num_instants_per_cycle = len(timeArr)
     for j in range(0, num_cycles_per_step):
         time = np.concatenate((time, timeArr+j*time_period))
