8bfc8c052a4a0185f2136dd04ad1da8627b71733
TheWeepingSage <kanabarmillen@gmail.com>
2019-07-08 15:35:54 +0530

    Corrections for old and new code

--- plot_simplified_new_state.py
+++ plot_simplified_new_state.py
@@ -27,13 +27,13 @@ array_true = np.loadtxt("simplified_actuator_true_long_data_3.csv", delimiter=",
 time_arr = np.linspace(0, 4, 4001, endpoint=False)
 array_old = np.loadtxt("simplified_actuator_tc_data_3.csv", delimiter=",")
 array_true_2 = np.loadtxt("simplified_actuator_true_data_6.csv", delimiter=",")
-#plt.plot(array_old[:, 0], array_old[:, 1], label="old w_0")
+plt.plot(array_old[:, 0], array_old[:, 1], label="old w_0")
 #plt.plot(array_new[:, 0], array_new[:, 1], label="new w_0")
-plt.plot(time_arr, array_true_2, label="true_old w_0")
-plt.plot(time_arr, array_true[:, :], label="true w_0")
+plt.plot(time_arr, -array_true_2, label="true_old w_0")
+#plt.plot(time_arr, array_true[:, 0]/(-25), label="true w_0")
 plt.legend(loc="upper left")
 plt.show()
-#w_final_tc = array_old[len(array_old)-1, 1:4]
-#w_final_true = array_true[len(array_true)-1, :]
-#factor = np.divide(w_final_true, w_final_tc)
-#print(factor)
+w_final_tc = array_old[len(array_old)-1, 1:4]
+w_final_true = array_true[len(array_true)-1, :]
+factor = np.divide(w_final_true, w_final_tc)
+print(factor)
