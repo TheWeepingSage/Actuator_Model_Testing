a66fbe874d940e53b747d7d82f0cb75b032ff2c1
TheWeepingSage <kanabarmillen@gmail.com>
2019-12-02 13:10:06 +0530

    Finished code for DC voltage to the torquers for the satellite suspended in free space

--- actuator_lr.py
+++ actuator_lr.py
@@ -7,7 +7,7 @@ def w_dot(v_w, torque):  # w_dot = I^-1(torque + w x Iw)
     inertia_matrix = np.array([[Ixx, Ixy, Ixz],
                                [Ixy, Iyy, Iyz],
                                [Ixz, Iyz, Izz]])
-    v_w_dot = np.linalg.inv(inertia_matrix)*(torque - np.cross(v_w, inertia_matrix*v_w))
+    v_w_dot = np.dot(np.linalg.inv(inertia_matrix), (torque - np.cross(v_w, np.dot(inertia_matrix, v_w))))
     return v_w_dot
 
 
@@ -18,29 +18,31 @@ def appliedTorque(v_current, b_inertial, v_q):  # torque = m x B(body)
 
 
 n = 5
-time_array = np.linspace(0, CONTROL_STEP, n*CONTROL_STEP*PWM_FREQUENCY + 1, endpoint=True)
+time_array = np.linspace(0, CONTROL_STEP, int(n*CONTROL_STEP*PWM_FREQUENCY) + 1, endpoint=True)
 stateArray = np.zeros((len(time_array), 7))
-stateArray[0] = np.array([0, 0, 0, 1, 0, 0, 0])
-v_b_applied = np.array([1, 2, 3]) * 1e-4    # b in the inertial frame
-v_duty_cycle = np.array([1, 1, 1]) * 1e-4   # duty cycle for the corresponding PWM
+stateArray[0] = np.array([0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0])
+v_b_applied = np.array([1.0, 2.0, 3.0]) * 1e-4    # b in the inertial frame
+v_duty_cycle = np.array([1.0, 1.0, 1.0]) * 1e-4   # duty cycle for the corresponding PWM
 v_v_applied = v_duty_cycle * PWM_AMPLITUDE  # v = v_max * duty cycle
 for i in range(len(time_array)-1):
-    state_i = stateArray[i]     # for indexing convenience
+    state_i = stateArray[i, :]     # for indexing convenience
     time_rk4 = np.linspace(time_array[i], time_array[i+1], 3, endpoint=True)    # for convenience in accessing values
-    h = time_array[i+1] - time_array[i]
-    i_array = np.zeros(3,3)
+    h = float(time_array[i+1] - time_array[i])
+    i_array = np.zeros((3, 3))
     factor_exp = (1-np.exp(-time_rk4*RESISTANCE/INDUCTANCE))
     i_array[:, 0] = v_v_applied[0] * factor_exp
     i_array[:, 1] = v_v_applied[1] * factor_exp     # i_k = V_k/R(1-e^(-Rt/L))
     i_array[:, 2] = v_v_applied[2] * factor_exp
-    k1_q = h * quatDerBI(state_i[0:4], stateArray[4:7])     # rk4 algorithm
-    k1_w = h * w_dot(state_i[4:6], appliedTorque(i_array[0], v_b_applied, state_i[0:4]))
-    k2_q = h * quatDerBI(state_i[0:4] + k1_q/2, state_i[4:7])
+    k1_q = quatDerBI(state_i[0:4], state_i[4:7]) * h     # rk4 algorithm
+    k1_w = h * w_dot(state_i[4:7], appliedTorque(i_array[0], v_b_applied, state_i[0:4]))
+    k2_q = quatDerBI(state_i[0:4] + k1_q/2.0, state_i[4:7]) * h
     k2_w = h * w_dot(state_i[4:7] + k1_w/2, appliedTorque(i_array[1], v_b_applied, state_i[0:4]))
-    k3_q = h * quatDerBI(state_i[0:4] + k2_q / 2, state_i[4:7])
+    k3_q = quatDerBI(state_i[0:4] + k2_q / 2.0, state_i[4:7]) * h
     k3_w = h * w_dot(state_i[4:7] + k2_w / 2, appliedTorque(i_array[1], v_b_applied, state_i[0:4]))
-    k4_q = h * quatDerBI(state_i[0:4] + k3_q, state_i[4:7])
+    k4_q = quatDerBI(state_i[0:4] + k3_q, state_i[4:7]) * h
     k4_w = h * w_dot(state_i[4:7] + k3_w, appliedTorque(i_array[2], v_b_applied, state_i[0:4]))
-    stateArray[i+1, 0:4] = state_i[0:4] + 1/6 * (k1_q + 2 * k2_q + 2 * k3_q + k4_q)     # update step
-    stateArray[i+1, 4:7] = state_i[4:7] + 1/6 * (k1_w + 2 * k2_w + 2 * k3_w + k4_w)
-np.savetxt("actuator_dc_state.csv", delimiter=",")
+    stateArray[i+1, 0:4] = state_i[0:4] + (k1_q + 2.0 * k2_q + 2.0 * k3_q + k4_q)/6.0  # update step for q
+    stateArray[i+1, 0:4] /= np.linalg.norm(stateArray[i+1, 1:4])    # normalising q
+    stateArray[i+1, 4:7] = state_i[4:7] + 1/6.0 * (k1_w + 2.0 * k2_w + 2.0 * k3_w + k4_w)     # update step for w
+np.savetxt("actuator_dc_state.csv", stateArray[:, :], delimiter=",")
+
--- plot_state.py
+++ plot_state.py
@@ -3,12 +3,18 @@ import matplotlib.pyplot as plt
 from constants_1U import CONTROL_STEP, PWM_FREQUENCY
 import analytical_act_current as aac
 
+'''
 v_duty_cycle = np.array([0.5, 0.7, 0.5])*1e-3
 time_arr = np.linspace(0, int(CONTROL_STEP), 2000001, endpoint=True)
 edge_current_array = aac.getEdgeCurrent(v_duty_cycle, I0=np.zeros(3))
 current_array = aac.getCurrentList(v_duty_cycle, time_arr, 2000001, edge_current_array)
-plt.plot(time_arr, current_array[:, 1])
-plt.ylabel("current")
-plt.title("Current vs Time for the actuator model")
+'''
+n=5
+time_array = np.linspace(0, CONTROL_STEP, int(n*CONTROL_STEP*PWM_FREQUENCY) + 1, endpoint=True)
+stateArray = np.loadtxt("actuator_dc_state.csv", delimiter=",")
+plt.plot(time_array, stateArray[:, 1])
+plt.plot(time_array, stateArray[:, 0])
+plt.ylabel("Quaternion component 1")
+plt.title("q_1 vs Time for the actuator model")
 plt.xlabel("time")
 plt.show()
